a4051ae6f2b34f8abb80c82a5399a03f002df60f
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2021-10-31 19:58:19 +0800

    Added pagination, search on frontend

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
+import Pagination from "react-js-pagination";
 import RoomItem from "./room/RoomItem";
 
 import { toast } from "react-toastify";
@@ -8,32 +11,70 @@ import { clearErrors } from "../redux/actions/roomActions";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
 
-  const { rooms, error } = useSelector((state) => state.allRooms);
+  const { rooms, resPerPage, roomsCount, filteredRoomsCount, error } =
+    useSelector((state) => state.allRooms);
+
+  let { location, page = 1 } = router.query;
+  page = Number(page);
 
   useEffect(() => {
     toast.error(error);
     dispatch(clearErrors());
   }, []);
 
-  return (
-    <section id="rooms" className="container mt-5">
-      <h2 className="mb-3 ml-2 stays-heading">Stays in New York</h2>
+  const handlePagination = (pageNumber) => {
+    window.location.href = `/?page=${pageNumber}`;
+  };
 
-      <a href="#" className="ml-2 back-to-search">
-        {" "}
-        <i className="fa fa-arrow-left"></i> Back to Search
-      </a>
-      <div className="row">
-        {rooms && rooms.length === 0 ? (
-          <div className="alert alert-danger">
-            <b>No Rooms</b>
-          </div>
-        ) : (
-          rooms && rooms.map((room) => <RoomItem key={room._id} room={room} />)
-        )}
-      </div>
-    </section>
+  let count = roomsCount;
+
+  if (location) {
+    count = filteredRoomsCount;
+  }
+
+  return (
+    <>
+      <section id="rooms" className="container mt-5">
+        <h2 className="mb-3 ml-2 stays-heading">
+          {location ? `Rooms in ${location}` : "All Rooms"}
+        </h2>
+
+        <Link href="/search">
+          <a className="ml-2 back-to-search">
+            <i className="fa fa-arrow-left"></i> Back to Search
+          </a>
+        </Link>
+        <div className="row">
+          {rooms && rooms.length === 0 ? (
+            <div className="alert alert-danger">
+              <b>No Rooms</b>
+            </div>
+          ) : (
+            rooms &&
+            rooms.map((room) => <RoomItem key={room._id} room={room} />)
+          )}
+        </div>
+      </section>
+
+      {resPerPage < count && (
+        <div className="d-flex justify-content-center mt-5">
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={resPerPage}
+            totalItemsCount={roomsCount}
+            onChange={handlePagination}
+            nextPageText={"Next"}
+            prevPageText={"Prev"}
+            firstPageText={"First"}
+            lastPageText={"Last"}
+            itemClass="page-item"
+            linkClass="page-link"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,20 @@ import Layout from "../components/layout/Layout";
 
 import { getRooms } from "../redux/actions/roomActions";
 
-import { wrapper } from '../redux/store'
+import { wrapper } from "../redux/store";
 
 export default function HomePage() {
   return (
     <Layout>
-      <Home/>
+      <Home />
     </Layout>
   );
 }
 
-
-export const getServerSideProps = wrapper.getServerSideProps(async({req, store}) => {
-  await store.dispatch(getRooms(req))
-})
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ req, query, store }) => {
+    await store.dispatch(
+      getRooms(req, query.page, query.location, query.guests, query.category)
+    );
+  }
+);
